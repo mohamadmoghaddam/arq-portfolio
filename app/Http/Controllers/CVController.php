bf91a37be870fa4ed7fa0183a5851832d3ea47b1
Gish <lordgishniz@gmail.com>
2024-08-01 21:53:56 +0330

    fix controller names typo

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\CV;
 use Illuminate\Http\Request;
 
-class CVConrtroller
+class CVController
 {
     public function index(){
 
--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Portfolio;
 use Illuminate\Http\Request;
 
-class PortfolioConrtroller
+class PortfolioController
 {
     public function index(){
         $projects = Portfolio::get();
