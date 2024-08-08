fe5378e8de47569546b9d5b030a1a005e0786c76
Gish <lordgishniz@gmail.com>
2024-08-08 18:08:13 +0330

    implement about page view and routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
 Route::get('/resume', [CVController::class, 'index']);
 
 Route::get('/portfolio', [PortfolioController::class, 'index']);
@@ -19,15 +23,18 @@ Route::get('/portfolio/{project}', [PortfolioController::class, 'show']);
 Route::get('/portfolio/category/{category}', [PortfolioController::class, 'category']);
 
 
-Route::get('/admin', [UserController::class, 'login']);
+Route::get('/admin', [UserController::class, 'login'])->name('login');
 Route::post('/admin/login', [UserController::class, 'authenticate']);
 
-Route::get('/admin/resume', [CVController::class, 'list']);
-Route::get('/admin/resume/create', [CVController::class, 'create']);
-Route::post('/admin/resume', [CVController::class, 'store']);
-Route::delete('/admin/resume/{cv}' ,[CVController::class, 'destroy']);
 
-Route::get('/admin/portfolio', [PortfolioController::class, 'list']);
-Route::get('/admin/portfolio/create', [PortfolioController::class, 'create']);
-Route::post('/admin/portfolio', [PortfolioController::class, 'store']);
-Route::delete('/admin/portfolio/{project}' ,[PortfolioController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::get('/admin/resume', [CVController::class, 'list']);
+    Route::get('/admin/resume/create', [CVController::class, 'create']);
+    Route::post('/admin/resume', [CVController::class, 'store']);
+    Route::delete('/admin/resume/{cv}' ,[CVController::class, 'destroy']);
+
+    Route::get('/admin/portfolio', [PortfolioController::class, 'list']);
+    Route::get('/admin/portfolio/create', [PortfolioController::class, 'create']);
+    Route::post('/admin/portfolio', [PortfolioController::class, 'store']);
+    Route::delete('/admin/portfolio/{project}' ,[PortfolioController::class, 'destroy']);
+});
