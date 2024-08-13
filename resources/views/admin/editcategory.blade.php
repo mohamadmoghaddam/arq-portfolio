1ad62c396f15bfb01151036519b576b9b4192d8f
Gish <lordgishniz@gmail.com>
2024-08-13 20:45:43 +0330

    fix edit category title

--- resources/views/admin/editcategory.blade.php
+++ resources/views/admin/editcategory.blade.php
@@ -31,7 +31,7 @@
                         <div class="col-lg-12">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Add a new category</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">Edit category</h1>
                                 </div>
                                 <form class="user" method="POST" action="/admin/category/{{$category['id']}}">
                                     @csrf
