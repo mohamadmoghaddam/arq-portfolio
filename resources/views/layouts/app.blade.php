b28490fc7b97beea821f8469660634b1cb48b38c
Gish <lordgishniz@gmail.com>
2024-08-15 17:37:01 +0330

    set up basic project clone from rez_portfolio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,15 @@
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Cardo:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
   <!-- Vendor CSS Files -->
   <link href="{{ URL::asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{ URL::asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
   <link href="{{ URL::asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
   <link href="{{ URL::asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ URL::asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
   <link href="{{ URL::asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
   <link href="{{ URL::asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
   <style>
@@ -48,7 +51,7 @@
 
   <!-- Template Main CSS File -->
   <link href="{{ URL::asset('assets/css/style.css') }}" rel="stylesheet">
-
+  <link href="{{ URL::asset('assets/css/main.css') }}" rel="stylesheet">
   <!-- =======================================================
   * Template Name: Personal
   * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
@@ -77,6 +80,7 @@
       <script src="{{ URL::asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
       <script src="{{ URL::asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
       <script src="{{ URL::asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+      <script src="{{ URL::asset('assets/vendor/aos/aos.js') }}"></script>
       <script src="{{ URL::asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
       <script src="{{ URL::asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
       <script src="{{ URL::asset('assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
